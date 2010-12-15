82f752d91aa3d3d7337a1d72d399f797e1c83b19
Bram <bram@fortfrances.com>
2010-12-15 14:44:37 -0600

    Added grid.

--- src/CelticKnot.java
+++ src/CelticKnot.java
@@ -1,17 +1,20 @@
-import javax.swing.JPanel;
+import javax.swing.JComponent;
 import java.awt.GridLayout;
-import java.awt.Dimension;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
 
-public class CelticKnot extends JPanel {
-
+public class CelticKnot extends JComponent {
+    private Line line = new BezierCurve();
+    private Grid grid;
+    private CelticNode[][] nodes;
+    private boolean showgrid = true;
+    
     public CelticKnot (int horizontalNodes, int verticalNodes) {
-        super(new GridLayout(horizontalNodes, verticalNodes, 0, 0));
+        super();
+        setLayout(new GridLayout(horizontalNodes, verticalNodes, 0, 0));
+        nodes = new CelticNode[horizontalNodes][verticalNodes];
         setBackground(Color.WHITE);
-        setPreferredSize(new Dimension(horizontalNodes * 40,
-                                       verticalNodes * 40));
-
-        Line l = new BezierCurve();
         
         boolean even_v = false;
         for (int y = 0; y < verticalNodes; y++) {
@@ -21,7 +24,8 @@ public class CelticKnot extends JPanel {
                 
                 int direction = even_h ? CelticNode.NORTHWEST_SOUTHEAST
                     : CelticNode.SOUTHWEST_NORTHEAST;
-                CelticNode node = new CelticNode(direction, l);
+                CelticNode node = new CelticNode(direction, line);
+                nodes[x][y] = node;
                 add(node);
                 if (y == 0) {
                     node.setBorder(CelticNode.NORTH, true);
@@ -37,6 +41,46 @@ public class CelticKnot extends JPanel {
             }
             even_v = !even_v;
         }
+
+        grid = new Grid(this);
+    }
+
+    public int getColumns() {
+        return ((GridLayout)getLayout()).getColumns();
+    }
+
+    public int getRows() {
+        return ((GridLayout)getLayout()).getRows();
+    }
+
+    public final CelticNode getNode (int x, int y) {
+        return nodes[x][y];
     }
     
+    public void paintComponent(Graphics g) {
+        g.setColor(getBackground());
+        g.fillRect(0, 0, getWidth(), getHeight());
+        if (showgrid)
+            grid.paint(g);
+    }
+
+    public final boolean isGridShown() {
+        return showgrid;
+    }
+    public void setShowGrid(boolean show) {
+        showgrid = show;
+        repaint();
+    }
+
+    public void setLine (Line line) {
+        Component[] c = getComponents();
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] instanceof CelticNode) {
+                ((CelticNode)c[i]).setLine(line);
+            }
+        }
+        // repaint the whole image
+        repaint();
+    }
 }
+
